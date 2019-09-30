f9d351e03d9f99d845a1b8bfb5950f9cb4fa6d29
Alyssa Farina <alyssafarina695@gmail.com>
2019-09-30 16:54:49 -0400

    started to add image to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 private
 
 def user_params
-    params.require(:user).permit(:name, :color, :current_location, :password)
+    params.require(:user).permit(:name, :color, :current_location, :password, :avatar)
 end
 
 end
